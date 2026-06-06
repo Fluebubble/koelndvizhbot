e44ad7990179624139c3b17b0bd9f17f558e95e0
Anatoliy Trots <Trots.anatoly@gmail.com>
2026-06-07 00:52:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ bot.on('text', async (ctx) => {
 });
 
 // Секретная команда проверки утреннего приветствия (только для админов)
-command('petuhPodjem', async (ctx) => {
+bot.command('petuhPodjem', async (ctx) => {
     const chatId = ctx.chat.id;
     const userId = ctx.from.id;
 
